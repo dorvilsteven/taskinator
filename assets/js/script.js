934410bf7310209b02c07ec9943162ae117b2e69
StevenD <dorvilsteven@gmail.com>
2021-08-28 01:08:43 -0400

    add todo list item to task list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-buttonEl.addEventListener("click", function() {
-  alert("button clicked");
-});
+var tasksToDoEl = document.querySelector("#task-to-do");
+
+var createTaskHandler = function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
