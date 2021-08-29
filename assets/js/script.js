b8f44f7f2cf56c49e17291d04cf272d0c9d4c035
StevenD <dorvilsteven@gmail.com>
2021-08-28 23:26:00 -0400

    flesh out createTaskHandler(), next step refactor the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,23 @@ var tasksToDoEl = document.querySelector("#task-to-do");
 
 var createTaskHandler = function(event) {
   event.preventDefault();
-  
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  // create container to hold list item and task info
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  
+  // add html content to container
+  taskInfoEl.innerHTML = `<h3 class='task-name'>${taskNameInput}</h3>
+                          <span class='task-type'>${taskTypeInput}</span>`;
+  // append container to the li
+  listItemEl.appendChild(taskInfoEl);
+  // append entire li to the task list(ul)
   tasksToDoEl.appendChild(listItemEl);
 }
 
